f7d46390aea3b06ccb0a77b399864fc699725201
maorhd03 <maorhd03@gmai.com>
2024-02-04 20:35:18 +0200

    New branch

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+const modules :any=[];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:modules,
+})
+export class MaterialModule { }
